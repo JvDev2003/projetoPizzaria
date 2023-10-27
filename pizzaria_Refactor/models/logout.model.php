50c7fc9d449bd37adfb1dd8735deeae9c9bdc639
JvDev2003 <jv.developer2003@gmail.com>
2023-10-27 16:24:32 -0300

    Logout
    Nova funcionalidade

---
+++ pizzaria_Refactor/models/logout.model.php
@@ -0,0 +1,4 @@
+<?php
+      session_start(); //inicia a sessão
+      session_destroy(); //destrói a sessão limpando todos os valores salvos
+      header("Location: /"); exit; //redireciona o visitante
