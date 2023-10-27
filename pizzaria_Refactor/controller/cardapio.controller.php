a3bbbddaaa351910a6cbdde9cb40de73247869d3
JvDev2003 <jv.developer2003@gmail.com>
2023-10-27 16:22:33 -0300

    Novas funcionalidades
    Fazer pedido e verificação de sessões

--- pizzaria_Refactor/controller/cardapio.controller.php
+++ pizzaria_Refactor/controller/cardapio.controller.php
@@ -2,8 +2,14 @@
 
     class CardapioController{
         public function printCardapio(){
+            require ('models/verificaSessao.model.php');
+            verifica_conectado();
             require ('layout/header.layout.php');
             require ('view/cardapio.view.php');
             require ('layout/footer.layout.php');
+        }
+
+        public function fazerPedido(){
+            require ('models/fazerPedido');
         }
     }
