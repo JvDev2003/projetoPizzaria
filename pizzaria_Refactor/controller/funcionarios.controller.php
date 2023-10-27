6af84d105e6fd5e63196e295da78a3ea7a9c0f9c
JvDev2003 <jv.developer2003@gmail.com>
2023-10-27 16:23:41 -0300

    Update funcionarios.controller.php

--- pizzaria_Refactor/controller/funcionarios.controller.php
+++ pizzaria_Refactor/controller/funcionarios.controller.php
@@ -4,7 +4,9 @@ class FuncionariosController{
         require ('layout/header.layout.php');
         require ('view/funcionarios.view.php');
         require ('layout/footer.layout.php');
-
+    }
+    public function entrarSistema(){
+        SistemaController::printSistema();
     }
 }
 /*require ('layout/header.layout.php');
