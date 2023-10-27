f0bed71c04c10c9a19dc519be76eabd9a581acc5
JvDev2003 <jv.developer2003@gmail.com>
2023-10-27 16:23:29 -0300

    Update funcionarios.view.php

--- pizzaria_Refactor/view/funcionarios.view.php
+++ pizzaria_Refactor/view/funcionarios.view.php
@@ -4,7 +4,7 @@
             <h2>Login para Funcionários</h2>
             <a href="./sistema.view.php">sistema</a>
             <a href="./php/fazerPedidoTeste.php">Teste</a>
-            <form method="post" action="php/efetuaLoginFuncionario.php">
+            <form method="post" action="./funcionario">
                 <label for="email">
 
                     <input type="email" name="email" id="email" placeholder="exemplo@email.com" />
