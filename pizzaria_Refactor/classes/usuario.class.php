6663ea7a5b54a95b3d41f25db5d3e5f30bab5a7f
JvDev2003 <jv.developer2003@gmail.com>
2023-10-29 16:18:49 -0300

    Usuarios
    Classe para controlar o cadastro de clientes

---
+++ pizzaria_Refactor/classes/usuario.class.php
@@ -0,0 +1,18 @@
+<?php 
+
+    class User {
+        public static function regitrarUsuario($dadosUsuario, $conn) {
+            $query = $conn->prepare("INSERT INTO login (email, senha, permissoes) VALUES ('{$dadosUsuario['email']}', '{$dadosUsuario['senha']}', 0)");
+            $query->execute();
+        }
+
+        public static function registrarDadosClientes($dadosUsuario, $conn) {
+            $query = $conn->prepare("INSERT INTO cliente (cpf, nome, fklogin) VALUES ({$dadosUsuario['cpf']}, '{$dadosUsuario['nome']}', '{$dadosUsuario['email']}')");
+            $query->execute();
+        }
+
+        public static function registrarDadosEndereco($dadosUsuario, $conn){
+            $query = $conn->prepare("INSERT INTO endereco (rua, cep, numero, fkCliente) VALUES ('{$dadosUsuario['rua']}', '{$dadosUsuario['cep']}', '{$dadosUsuario['numero']}', {$dadosUsuario['cpf']})");
+            $query->execute();
+        }
+    }
