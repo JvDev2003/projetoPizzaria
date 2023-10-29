e6a91dff50dbd0897c285dfa4c29a958e8a21170
JvDev2003 <jv.developer2003@gmail.com>
2023-10-29 16:22:31 -0300

    Registrar
    logica por trás de registrar

---
+++ pizzaria_Refactor/models/registrar.model.php
@@ -0,0 +1,13 @@
+<?php
+
+    $conn = Conexao::get();
+    $conn->beginTransaction();
+
+    User::regitrarUsuario($_POST, $conn);
+    User::registrarDadosEndereco($_POST, $conn);
+    User::registrarDadosClientes($_POST, $conn);
+
+    $conn->commit();
+
+    //header("location: /login");
+
