ca481e52715601809a636105f257b79f611ea461
JvDev2003 <jv.developer2003@gmail.com>
2023-10-31 15:54:09 -0300

    Novo alert

---
+++ pizzaria_Refactor/alerts/cadastrado.php
@@ -0,0 +1,4 @@
+<script>
+    let node = document.getElementById('alertLogin');
+    node.appendChild(document.createTextNode('Cadastro realizado com sucesso, agora você so precisa fazer o login.'));
+</script>
